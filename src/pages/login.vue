f2d98d3330fc86e4cf857bd2413d890126882220
xlk <1148038404@qq.com>
2023-05-21 22:43:48 +0800

    修改login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 const router = useRouter();
+const loginForm = reactive({
+  username: 'xxx',
+  password: '123456'
+})
+
 </script>
 
 <template>
@@ -41,7 +46,8 @@ const router = useRouter();
                   type="email"
                   name="email"
                   id="email"
-                  placeholder="example@example.com"
+                  v-model="loginForm.username"
+                  placeholder=""
                   class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
@@ -64,7 +70,8 @@ const router = useRouter();
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="Your Password"
+                  placeholder=""
+                  v-model="loginForm.password"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                 />
               </div>
@@ -93,6 +100,7 @@ const router = useRouter();
     </div>
   </div>
 </template>
+
 
 <route lang="yaml">
 meta:
